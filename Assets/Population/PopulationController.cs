1dad93c8dfa083ae93e9128a46d5835c24b1fab8
gletschr <kostarevgi@gmail.com>
2018-10-20 18:01:11 +0700

    added population controller

---
+++ Assets/Population/PopulationController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationController : MonoBehaviour {
+
+    public GameObject monadPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+            SpawnRandomMonad(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+	}
+
+    void SpawnRandomMonad(Vector3 position) {
+        GameObject monad = Instantiate(monadPrefab);
+        monad.transform.position = new Vector3(position.x, position.y, 0.0f);
+    }
+
+}
